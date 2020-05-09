788fb844f8ef107d13dda6f612249888dc74f8a4
friilancer <jirapzo7@gmail.com>
2020-05-10 00:02:48 +0100

    added a response message

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,8 @@
 document.getElementById('formControlOpen').onclick = () => {
     let form = document.getElementById('formContainer');
+    console.log(document.body.style.width);
     form.classList.remove('hide');
     document.body.classList.add('disable');
-
 }
 
 document.getElementById('formControlClose').onclick = () =>{
@@ -17,27 +17,27 @@ document.getElementById('submit').onclick = () =>{
     let name= document.getElementById('name').value.trim().toString();
     let email= document.getElementById('email').value.trim().toString().toLowerCase();
     let text= document.getElementById('textarea').value.trim().toString();
-    
+    form.classList.add('hide');    
     let obj ={name:name, email:email, text:text};
-        document.getElementById('name').value = '';
-        document.getElementById('email').value='';
-        document.getElementById('textarea').value='';
-        document.body.classList.remove('disable');
-        form.classList.add('hide');
-        fetch(`/`, {
-            headers:{
-                'Content-Type':'application/json'
+    document.getElementById('name').value = '';
+    document.getElementById('email').value='';
+    document.getElementById('textarea').value='';
+    document.body.classList.remove('disable');
+    fetch(`/`, {
+        headers:{
+            'Content-Type':'application/json'
             },
-            method : 'POST',
-            body: JSON.stringify(obj)
-        }).then((res) =>{
-          return res.json()
-        }).then((data)=>{
-            console.log(data.text);
-        }).catch(err => {
-            throw err;
-        })
-    
-    
+        method : 'POST',
+        body: JSON.stringify(obj)
+    }).then((res) =>{
+      return res.json()
+    }).then((data)=>{
+        document.getElementById('responseMessage').textContent = data.text;
+    }).catch(err => {
+        throw err;
+    })
+    setTimeout(()=>{
+        document.getElementById('responseMessage').classList.add('hide');
+     }, 2000)   
     
 }
