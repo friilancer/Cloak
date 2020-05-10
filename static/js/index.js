4509a056b6908b38d90ed87167317aeb00b387b6
friilancer <jirapzo7@gmail.com>
2020-05-10 10:20:02 +0100

    added dynamic responses for form and input validation

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 document.getElementById('formControlOpen').onclick = () => {
     let form = document.getElementById('formContainer');
-    console.log(document.body.style.width);
     form.classList.remove('hide');
     document.body.classList.add('disable');
 }
@@ -16,28 +15,41 @@ document.getElementById('submit').onclick = () =>{
     event.preventDefault();
     let name= document.getElementById('name').value.trim().toString();
     let email= document.getElementById('email').value.trim().toString().toLowerCase();
-    let text= document.getElementById('textarea').value.trim().toString();
-    form.classList.add('hide');    
-    let obj ={name:name, email:email, text:text};
+    let text= document.getElementById('textarea').value.trim().toString();    
+    const obj = {name:name, email:email, text:text};
+    for(let data in obj){
+        if(obj[data].length == 0){
+            document.getElementById('formError').textContent = `Please fill in ${data}`;
+            return;
+        }
+
+        else if(obj[data].length < 3){
+            document.getElementById('formError').textContent = `${data} provided seems incomplete`;
+            return;
+        }
+    }
+
     document.getElementById('name').value = '';
     document.getElementById('email').value='';
     document.getElementById('textarea').value='';
-    document.body.classList.remove('disable');
     fetch(`/`, {
         headers:{
             'Content-Type':'application/json'
             },
         method : 'POST',
         body: JSON.stringify(obj)
-    }).then((res) =>{
-      return res.json()
-    }).then((data)=>{
-        document.getElementById('responseMessage').textContent = data.text;
+    }).then((res) =>
+        res.json()
+    ).then((data)=>{
+        document.getElementById('formError').textContent = 'Message Received';
+        console.log(data.text);
     }).catch(err => {
         throw err;
     })
-    setTimeout(()=>{
-        document.getElementById('responseMessage').classList.add('hide');
-     }, 2000)   
     
+    setTimeout(()=>{
+        form.classList.add('hide');
+        document.body.classList.remove('disable');
+        document.getElementById('formError').textContent = '';
+     }, 3000);   
 }
